20f0cffb9e5c3aa762f11706c10022977a00e446
core.pager=cat- <ela.mannai@etudiant-fst.utm.tn>
2022-03-26 11:00:56 +0100

    salla7t fl timer

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,6 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 
 const Profile = () => {
+    const [timer , setTimer]=useState(1);
+  
+    useEffect(()=>{
+      const intervalID=setInterval(()=>{
+        setTimer(timer=>timer+1);
+        console.log("Hello interval");
+      }, 1000)
+      return()  => {
+        clearInterval(intervalID);
+      }
+      },[])
+      
   const [Name, setName] = useState("Ela");
 
   useEffect(() => {
@@ -20,6 +32,8 @@ const Profile = () => {
   return (
     <div>
       <div>
+      <h1>Chrono : {timer} </h1>
+
         <Card className="cerd" style={{ width: "18rem" }}>
           <Card.Img className="img" variant="top" src="ela.png" />
           <Card.Body>
